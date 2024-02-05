db192eb00219acdfa50f0c62bce28baa4f7f142d
unknown <AbstractProgrammer>
2024-02-05 22:37:33 +0530

    first commit

---
+++ project/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './Components/Login-signup/Loginsignup';
+
+function App() {
+  return (
+    <div>
+      <LoginSignup/> 
+    </div>
+  );
+}
+
+export default App;
